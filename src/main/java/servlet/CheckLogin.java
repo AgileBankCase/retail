47c1a000b2202bcc68b9382c1697eb990de0ce8b
DhineshDhino <k.dhineshasan@gmail.com>
2020-06-17 03:15:13 +0530

    Login session logout done

---
+++ src/main/java/servlet/CheckLogin.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.LoginDAO;
+import service.Validator;
+
+@WebServlet(
+        name = "logincheck",
+        urlPatterns = {"/logincheck"}
+    )
+public class CheckLogin extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    	HttpSession session=req.getSession();
+    	if(session.getAttribute("uname")!=null||session.getAttribute("uname")=="") {
+    		if(session.getAttribute("type").equals("executive")) {
+    			RequestDispatcher requestDispatcher = req.getRequestDispatcher("customer search.html");
+        		requestDispatcher.forward(req, resp);
+    		}
+    		else if(session.getAttribute("type").equals("cashier")) {
+    			RequestDispatcher requestDispatcher = req.getRequestDispatcher("transfer money.html");
+        		requestDispatcher.forward(req, resp);
+    		}
+    	}
+    	else {
+    		RequestDispatcher requestDispatcher = req.getRequestDispatcher("loginscreen.jsp");
+    		requestDispatcher.forward(req, resp);
+    	}
+    	
+    }
+
+}
--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.LoginDAO;
 import service.Validator;
@@ -28,14 +29,22 @@ public class HelloServlet extends HttpServlet {
     	String accountType="false";
     	if(Validator.isValidString(username) && Validator.isValidString(password)) { 
     		accountType = LoginDAO.checkCredentials(username,password);
-    		if(Validator.isValidString(accountType)&&accountType.equals("executive")) {
-    			RequestDispatcher requestDispatcher = req.getRequestDispatcher("customer search.html");
-        		requestDispatcher.forward(req, resp);
+    		
+    		if(Validator.isValidString(accountType)) {
+    			HttpSession session=req.getSession();
+    			session.setAttribute("uname", username);
+        		session.setAttribute("type", accountType);
+        		
+        		if(accountType.equals("executive")) {
+        			RequestDispatcher requestDispatcher = req.getRequestDispatcher("customer search.html");
+            		requestDispatcher.forward(req, resp);
+        		}
+        		else if(accountType.equals("cashier")) {
+        			RequestDispatcher requestDispatcher = req.getRequestDispatcher("transfer money.html");
+            		requestDispatcher.forward(req, resp);
+        		}
     		}
-    		else if(Validator.isValidString(accountType)&&accountType.equals("cashier")) {
-    			RequestDispatcher requestDispatcher = req.getRequestDispatcher("transfer money.html");
-        		requestDispatcher.forward(req, resp);
-    		}
+    		
     		else if(accountType.equals("mismatch")){
         		req.setAttribute("msg", "Username or Password not correct!");
         		RequestDispatcher requestDispatcher = req.getRequestDispatcher("loginscreen.jsp");
